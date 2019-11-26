cdc73930a9fdfb1218cb930546992c154836654f
Gavin Cheng <cwmima@hotmail.com>
2019-11-27 10:01:24 +1300

    init

--- configgenerator.py
+++ configgenerator.py
@@ -1,9 +1,9 @@
 import json
 
 jsondata = {
-    "word2vecfile": "/home/omri/datasets/word2vec/GoogleNews-vectors-negative300.bin",
-    "choidataset": "/home/omri/code/text-segmentation-2017/data/choi",
-    "wikidataset": "/home/omri/datasets/wikipedia/process_dump_r",
+    "word2vecfile": "./data/word2vec/GoogleNews-vectors-negative300.bin",
+    "choidataset": "./data/choi",
+    "wikidataset": "./data/wikipedia",
 }
 
 with open('config.json', 'w') as f:
